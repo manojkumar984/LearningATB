82ea9f875d55d5e07955c4b0a899ef785ffe3247
manojkumar <gollapallimanoj66@gmail.com>
2025-05-14 11:46:34 +0530

    11 may 2025 part 1

---
+++ src/ex_02_java_basics_part2/codingtask2.java
@@ -0,0 +1,27 @@
+package ex_02_java_basics_part2;
+
+public class codingtask2 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        System.out.printf("10 * 1 = %d",num);
+        System.out.println();
+        System.out.printf("10 * 2 = %d",num*2);
+        System.out.println();
+        System.out.printf("10 * 3 = %d",num*3);
+        System.out.println();
+        System.out.printf("10 * 4 = %d",num*4);
+        System.out.println();
+        System.out.printf("10 * 5 = %d",num*5);
+        System.out.println();
+        System.out.printf("10 * 6 = %d",num*6);
+        System.out.println();
+        System.out.printf("10 * 7 = %d",num*7);
+        System.out.println();
+        System.out.printf("10 * 8 = %d",num*8);
+        System.out.println();
+        System.out.printf("10 * 9 = %d",num*9);
+        System.out.println();
+        System.out.printf("10 * 10 = %d",num*10);
+    }
+}
